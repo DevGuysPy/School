0f820eb2575016dd47d6b4a7e38122b1980ac4e1
andrew <andrey.cplusplus@gmail.com>
2015-08-03 00:57:28 +0300

    Fix MEDIA_URL

--- School/settings.py
+++ School/settings.py
@@ -118,12 +118,15 @@ else:
     SERVE_MEDIA = True
 
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+if AWS_STORAGE_BUCKET_NAME:
+    MEDIA_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+else:
+    MEDIA_URL = '/media/'
 
 
 TEMPLATE_DIRS = (
